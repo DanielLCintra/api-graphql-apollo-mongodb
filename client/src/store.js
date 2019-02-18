ea5096dcc7be7345fe1994d04e7dde0d755c0a4f
Daniel Loureiro Cintra <daniel.cintra@Daniels-MacBook-Pro.local>
2019-02-18 16:53:40 -0300

    Utilizando o vuex para obtensão de dados

--- client/src/store.js
+++ client/src/store.js
@@ -1,16 +1,48 @@
-import Vue from 'vue'
-import Vuex from 'vuex'
+import Vue from "vue";
+import Vuex from "vuex";
 
-Vue.use(Vuex)
+import { defaultClient as apolloClient } from "./main";
+
+import { GET_POSTS } from "./queries";
+
+Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {
-
+    posts: [],
+    loading: false
   },
+
   mutations: {
+    SET_POSTS: (state, payload) => {
+      state.posts = payload;
+    },
 
+    SET_LOADING: (state, payload) => {
+      state.loading = payload;
+    }
   },
-  actions: {
 
+  actions: {
+    getPosts: ({ commit }) => {
+      commit("SET_LOADING", true);
+      apolloClient
+        .query({
+          query: GET_POSTS
+        })
+        .then(({ data: { getPosts } }) => {
+          commit("SET_POSTS", getPosts);
+          commit("SET_LOADING", false);
+        })
+        .catch(error => {
+          commit("SET_LOADING", false);
+          console.error(error);
+        });
+    }
+  },
+
+  getters: {
+    posts: state => state.posts,
+    loading: state => state.loading
   }
-})
+});
