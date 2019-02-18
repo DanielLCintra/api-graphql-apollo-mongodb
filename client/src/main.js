f4928c8250353eab6bf88573c6d20f9fd5e5a16d
Daniel Loureiro Cintra <daniel.cintra@Daniels-MacBook-Pro.local>
2019-02-18 10:03:16 -0300

    Atualizando pacotes e resolvendo problemas

--- client/src/main.js
+++ client/src/main.js
@@ -1,13 +1,25 @@
-import Vue from 'vue'
-import './plugins/vuetify'
-import App from './App.vue'
-import router from './router'
-import store from './store'
+import Vue from "vue";
+import "./plugins/vuetify";
+import App from "./App.vue";
+import router from "./router";
+import store from "./store";
 
-Vue.config.productionTip = false
+import ApolloClient from "apollo-boost";
+import VueApollo from "vue-apollo";
+
+Vue.use(VueApollo);
+
+const apolloClient = new ApolloClient({
+  uri: "http://localhost:4000/graphql"
+});
+
+const apolloProvider = new VueApollo({ apolloClient });
+
+Vue.config.productionTip = false;
 
 new Vue({
+  apolloProvider,
   router,
   store,
   render: h => h(App)
-}).$mount('#app')
+}).$mount("#app");
